351f43a6106c88734e09264f2c7ca2b5b7972334
Sneha Biju Kurup <122629654+snehaa011@users.noreply.github.com>
2024-06-20 11:08:15 +0400

    Update Jun19.cpp

--- Jun19.cpp
+++ Jun19.cpp
@@ -1,17 +1,4 @@
 class Solution {
-    bool isboq(int d, int k, int m, vector<int> arr){
-        int bm=0, count=0;
-        for (int i=0; i<arr.size(); i++){
-            if (arr[i]<=d) count++;
-            else count=0;
-            if (count==k){
-                bm++;
-                count=0;
-            }
-            if (bm>=m) return true;
-        }
-        return false;
-    }
 public:
     int minDays(vector<int>& bloomDay, int m, int k) {
         if (bloomDay.size()<(long)m*k) return -1;
@@ -19,9 +6,21 @@ public:
         int l=1;
         while (l<r){
             int mid=(l+r)/2;
-            if (isboq(mid, k, m, bloomDay)) r=mid;
+            int count=0, bm=m;
+            for (int i=0; i<bloomDay.size(); i++){
+                if (bloomDay[i]<=mid) count++;
+                else count=0;
+                if (count==k){
+                    bm--;
+                    count=0;
+                }
+            }
+            if (bm<=0) r=mid;
             else l=mid+1;
         }
         return l;
     }
 };
+
+//T(n) = O(nlogn)
+//S(n) = O(1)
