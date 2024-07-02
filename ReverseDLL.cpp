540dc42ea45acc7bc86117fbb0fc4c3245968ebd
Sneha Biju Kurup <122629654+snehaa011@users.noreply.github.com>
2024-07-02 11:12:13 +0400

    reverse DLL

---
+++ ReverseDLL.cpp
@@ -0,0 +1,16 @@
+//{ Driver Code Starts
+#include <stdio.h>
+#include <stdlib.h>
+
+/* a Node of the doubly linked list */
+struct Node {
+    int data;
+    struct Node *next;
+    struct Node *prev;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+        prev = NULL;
+    }
+};
